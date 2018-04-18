a6bf6188afd6e91dcd84f4cdf2336eb7ff78add5
NHibiki <i@nekoyu.cc>
2018-04-18 16:30:38 +0800

    SW

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainActivity from './lib/MainActivity.js';
 import './initial.css';
+import './registerServiceWorker';
 
 ReactDOM.render(<MainActivity />, document.getElementById("root"));
